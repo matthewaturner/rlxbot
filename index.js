1d5d50b8015d58696023454ff4c83fc12817da72
matthewaturner <matthewallenturner@gmail.com>
2019-03-26 23:46:38 -0700

    getting comments and replying

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 const http = require('http');
+const snoowrap = require('snoowrap');
+const snoostorm = require('snoostorm');
 
+// import environment variables if they exist
+require('dotenv').config();
+
+
+const client = new snoowrap({
+    userAgent: 'rxlbot-node',
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    username: process.env.REDDIT_USER,
+    password: process.env.REDDIT_PASS
+});
+
+const streamOpts = {
+    subreddit: 'testingground4bots',
+    limit: 5,
+    pollTime: 20000
+};
+
+const comments = new snoostorm.CommentStream(client, streamOpts);
+
+comments.on('item', (comment) => {
+    console.log('Got a comment: \'%s\'', comment.body);
+    if (comment.body === ':(') {
+        console.log('Replied!');
+        comment.reply(':)');
+    }
+});
+
+// just so we know the bot is running
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Azure!");
+    response.end("RLXBot is working!");
 });
 
 const port = process.env.PORT || 1337;

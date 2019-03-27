5ca2edbc3006f11766e499667370bf4460524c0f
matthewaturner <matthewallenturner@gmail.com>
2019-03-27 15:06:05 -0700

    checking body of posts

--- filter.js
+++ filter.js
@@ -1,9 +1,16 @@
 
 function filter(post) {
 	var title = post.title.toLowerCase();
+	var body = "";
 
-	if (title.indexOf('[xbox]') >= 0 && title.indexOf('halo') >= 0) {
-		return true;
+	if (post.body !== undefined) {
+		body = post.body.toLowerCase();
+	}
+
+	if (title.indexOf('[xbox]') >= 0) {
+		if (title.indexOf('halo') >= 0 || body.indexOf('halo') >= 0) {
+			return true;
+		}
 	}
 
 	return false;
